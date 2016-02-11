805873d76f9e51cf7c46eb80581e59403a488745
todorm85 <todorbm85@gmail.com>
2016-02-11 18:55:56 +0200

    Remove old project files

---
+++ Source/Data/HikingPlanAndRescue.Data.Models/Track.cs
@@ -0,0 +1,31 @@
+﻿namespace HikingPlanAndRescue.Data.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using Common.Models;
+
+    public class Track : BaseModel<int>
+    {
+        public Track()
+        {
+            this.CreatedOn = DateTime.UtcNow;
+        }
+
+        public string Title { get; set; }
+
+        public string UserId { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+
+        //[Required]
+        //public string FilePath { get; set; }
+
+        public double Length { get; set; }
+
+        public double AvgAscentInclination { get; set; }
+
+        public double TotalAscent { get; set; }
+
+        //public enum TrackCategory { get; set; }
+    }
+}
