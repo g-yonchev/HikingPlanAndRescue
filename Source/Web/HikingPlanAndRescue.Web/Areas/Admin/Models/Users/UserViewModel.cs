af47b87617fdae8d3578085fb5b3d1a6231754be
todorm85 <todorbm85@gmail.com>
2016-02-14 21:34:29 +0200

    Add _Layout for GridMvc in Administrators area

--- Source/Web/HikingPlanAndRescue.Web/Areas/Admin/Models/Users/UserViewModel.cs
+++ Source/Web/HikingPlanAndRescue.Web/Areas/Admin/Models/Users/UserViewModel.cs
@@ -8,15 +8,13 @@
 
     public class UserViewModel : IMap<ApplicationUser>
     {
+        public string Id { get; set; }
+
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
 
         public string GSM { get; set; }
-
-        public bool IsDeleted { get; set; }
-
-        public DateTime? DeletedOn { get; set; }
 
         public DateTime CreatedOn { get; set; }
 
